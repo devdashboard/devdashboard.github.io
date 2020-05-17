4fee5b98010f0c0e8b58a7353fa3245f52c77039
devdashboard <64152133+devdashboard@users.noreply.github.com>
2020-05-17 14:00:48 +0530

    Revert the script and push the latest one

--- script/custom-script.js
+++ script/custom-script.js
@@ -1,226 +1,3 @@
-function getSensexIndex() {
-
-	$.ajax({
-		method: "GET",
-		url: "https://priceapi-aws.moneycontrol.com/pricefeed/notapplicable/inidicesindia/in%3BSEN",
-		success: function (resData) {
-
-			var actualData = resData["data"];
-
-			if (actualData["company"] === 'SENSEX') {
-				valueTemplate = '<tr><td class="header-confirmed numberFirst">' + numberWithCommas(actualData["company"]).toUpperCase() + '</td>' +
-					'<td class="header-cprice numberValue">' + numberWithCommas(actualData["pricecurrent"]) + '</td>' +
-					'<td class="header-dhigh numberValue">' + numberWithCommas(actualData["HIGH"]) + '</td>' +
-					'<td class="header-dlow numberValue">' + numberWithCommas(actualData["LOW"]) + '</td>' +
-					'<td class="header-change numberValue">' + numberWithCommas(actualData["PERCCHANGE"]) + '%</td></tr>';
-				$("#table-body").append(valueTemplate);
-			}
-		},
-		error: function (xhr, status, error) {
-			alert(error);
-		}
-	});
-}
-
-function getAsianIndices() {
-	$.ajax({
-		method: "GET",
-		url: "https://quote.cnbc.com/quote-html-webservice/quote.htm?noform=1&partnerId=2&fund=1&exthrs=0&output=json&symbols=.N225|.SSEC|.SZSC|.HSI|.AXJO|.KS11|.STI|.NZ50|.NSEI|.SETI|.JKSE|.KLSE|.TWII&requestMethod=extended",
-		success: function (resData) {
-
-			var loopData = resData["ExtendedQuoteResult"]["ExtendedQuote"];
-			loopData.forEach(function (item) {
-				var actualData = item["QuickQuote"];
-				valueTemplate = '<tr><td class="header-confirmed numberFirst">' + actualData["shortName"].toUpperCase() + '</td>' +
-					'<td class="header-cprice numberValue">' + actualData["last"] + '</td>' +
-					'<td class="header-dhigh numberValue">' + actualData["high"] + '</td>' +
-					'<td class="header-dlow numberValue">' + actualData["low"] + '</td>' +
-					getFormattedChangePercent(actualData["change_pct"]) + '</tr>';
-				$("#table-body").append(valueTemplate);
-			});
-			sortDataByAscOrder('table-body');
-		},
-		error: function (xhr, status, error) {
-			alert(error);
-		}
-	});
-}
-
-function getAmericanIndices() {
-	$.ajax({
-		method: "GET",
-		url: "https://quote.cnbc.com/quote-html-webservice/quote.htm?noform=1&partnerId=2&fund=1&exthrs=0&output=json&symbols=.DJI|.IXIC|.SPX|.RUT|.VIX|.GSPTSE|.SPTSE|.MXX|.BVSP|.MERV|.IPSA|.IBC&requestMethod=extended",
-		success: function (resData) {
-
-			var loopData = resData["ExtendedQuoteResult"]["ExtendedQuote"];
-			loopData.forEach(function (item) {
-				var actualData = item["QuickQuote"];
-				valueTemplate = '<tr><td class="header-confirmed numberFirst">' + actualData["shortName"].toUpperCase() + '</td>' +
-					'<td class="header-cprice numberValue">' + actualData["last"] + '</td>' +
-					'<td class="header-dhigh numberValue">' + actualData["high"] + '</td>' +
-					'<td class="header-dlow numberValue">' + actualData["low"] + '</td>' +
-					getFormattedChangePercent(actualData["change_pct"]) + '</tr>';
-				$("#table-body").append(valueTemplate);
-			});
-			sortDataByAscOrder('table-body');
-		},
-		error: function (xhr, status, error) {
-			alert(error);
-		}
-	});
-}
-
-function getEuropeanIndices() {
-	$.ajax({
-		method: "GET",
-		url: "https://quote.cnbc.com/quote-html-webservice/quote.htm?noform=1&partnerId=2&fund=1&exthrs=0&output=json&symbols=.FTSE|.GDAXI|.FCHI|.STOXX|.AEX|.BFX|.PSI20|.IMOEX|.FTMIB|.SSMI|.OBX|.OMXHPI|.OMXS30|.ATG|.OMXC25CAP&requestMethod=extended",
-		success: function (resData) {
-
-			var loopData = resData["ExtendedQuoteResult"]["ExtendedQuote"];
-			loopData.forEach(function (item) {
-				var actualData = item["QuickQuote"];
-				valueTemplate = '<tr><td class="header-confirmed numberFirst">' + actualData["shortName"].toUpperCase() + '</td>' +
-					'<td class="header-cprice numberValue">' + actualData["last"] + '</td>' +
-					'<td class="header-dhigh numberValue">' + actualData["high"] + '</td>' +
-					'<td class="header-dlow numberValue">' + actualData["low"] + '</td>' +
-					getFormattedChangePercent(actualData["change_pct"]) + '</tr>';
-				$("#table-body").append(valueTemplate);
-			});
-			sortDataByAscOrder('table-body');
-		},
-		error: function (xhr, status, error) {
-			alert(error);
-		}
-	});
-}
-
-
-function getNseIndices(indexCode) {
-	$.ajax({
-
-		method: "GET",
-		url: "https://priceapi-aws.moneycontrol.com/pricefeed/notapplicable/inidicesindia/" + indexCode,
-		success: function (resData) {
-
-			var actualData = resData["data"];
-
-			//Populate timestamp only once in iteration
-			if (indexCode === "in%3Bccx") {
-				$("#updatedTimeNse").append(computeUpdatedTime(actualData["lastupd"]));
-			}
-
-			valueTemplate = '<tr><td class="header-confirmed numberFirst">' + numberWithCommas(actualData["company"]).toUpperCase() + '</td>' +
-				'<td class="header-cprice numberValue">' + numberWithCommas(actualData["pricecurrent"]) + '</td>' +
-				'<td class="header-dhigh numberValue">' + numberWithCommas(actualData["HIGH"]) + '</td>' +
-				'<td class="header-dlow numberValue">' + numberWithCommas(actualData["LOW"]) + '</td>' +
-				getFormattedChangePercent(actualData["PERCCHANGE"]) + '</tr>';
-			$("#table-body").append(valueTemplate);
-			sortDataByAscOrder('table-body');
-		},
-		error: function (xhr, status, error) {
-			alert(error);
-		}
-	});
-}
-
-
-function getBseIndices(indexCode) {
-	$.ajax({
-
-		method: "GET",
-		url: "https://priceapi-aws.moneycontrol.com/pricefeed/notapplicable/inidicesindia/" + indexCode,
-		success: function (resData) {
-
-			var actualData = resData["data"];
-
-			//Populate timestamp only once in iteration
-			if (indexCode === "in%3Bccx") {
-				$("#updatedTimeNse").append(computeUpdatedTime(actualData["lastupd"]));
-			}
-
-			valueTemplate = '<tr><td class="header-confirmed numberFirst">' + numberWithCommas(actualData["company"]).toUpperCase() + '</td>' +
-				'<td class="header-cprice numberValue">' + numberWithCommas(actualData["pricecurrent"]) + '</td>' +
-				'<td class="header-dhigh numberValue">' + numberWithCommas(actualData["HIGH"]) + '</td>' +
-				'<td class="header-dlow numberValue">' + numberWithCommas(actualData["LOW"]) + '</td>' +
-				getFormattedChangePercent(actualData["PERCCHANGE"]) + '</tr>';
-			$("#table-body").append(valueTemplate);
-			sortDataByAscOrder('table-body');
-		},
-		error: function (xhr, status, error) {
-			alert(error);
-		}
-	});
-}
-
-function getNiftyIndices() {
-	$.ajax({
-		method: "GET",
-		url: "https://etmarketsapis.indiatimes.com/ET_Stats/getIndexByIds?pagesize=30&exchange=47&sortby=currentprice&sortorder=desc&indexid=2369&company=true&marketcap=",
-		success: function (resData) {
-
-			var searchResult = resData["searchresult"];
-			searchResult.forEach(function (companies) {
-				for (i = 0; i < companies["companies"].length; i++) {
-					var actualData = companies["companies"][i];
-					valueTemplate = '<tr><td class="header-confirmed numberFirst">' + actualData["symbol"].toUpperCase() + '</td>' +
-						'<td class="header-cprice numberValue">' + actualData["current"] + '</td>' +
-						'<td class="header-dhigh numberValue">' + actualData["high"] + '</td>' +
-						'<td class="header-dlow numberValue">' + actualData["low"] + '</td>' +
-						getFormattedChangePercent(actualData["percentChange"]) + '</tr>';
-
-					$("#table-body").append(valueTemplate);
-				}
-			});
-			sortDataByAscOrder('table-body');
-		},
-		error: function (xhr, status, error) {
-			alert(error);
-		}
-	});
-}
-
-function getSensexIndices() {
-	$.ajax({
-		method: "GET",
-		url: "https://etmarketsapis.indiatimes.com/ET_Stats/getIndexByIds?pagesize=30&exchange=47&sortby=currentprice&sortorder=desc&indexid=2365&company=true&marketcap=",
-		success: function (resData) {
-
-			var searchResult = resData["searchresult"];
-			searchResult.forEach(function (companies) {
-				for (i = 0; i < companies["companies"].length; i++) {
-					var actualData = companies["companies"][i];
-					valueTemplate = '<tr><td class="header-confirmed numberFirst">' + actualData["symbol"].toUpperCase() + '</td>' +
-						'<td class="header-cprice numberValue">' + actualData["current"] + '</td>' +
-						'<td class="header-dhigh numberValue">' + actualData["high"] + '</td>' +
-						'<td class="header-dlow numberValue">' + actualData["low"] + '</td>' +
-						getFormattedChangePercent(actualData["percentChange"]) + '</tr>';
-
-					$("#table-body").append(valueTemplate);
-				}
-			});
-			sortDataByAscOrder('table-body');
-		},
-		error: function (xhr, status, error) {
-			alert(error);
-		}
-	});
-}
-
-
-function getFormattedChangePercent(chngPrcnt) {
-	var formattedPrcnt;
-	if (chngPrcnt >= 0) {
-
-		formattedPrcnt = '<td class="header-change numberValueGreen">' + chngPrcnt + '%</td>';
-	}
-	else {
-		formattedPrcnt = '<td class="header-change numberValueRed">' + chngPrcnt + '%</td>';
-	}
-
-	return formattedPrcnt;
-}
-
-
 function formatDate(date) {
 	var utc = date.toUTCString() // 'ddd, DD MMM YYYY HH:mm:ss GMT'
 	return utc.slice(8, 12) + utc.slice(5, 7) + ", " + utc.slice(12, 16)
@@ -234,37 +11,53 @@ function numberWithCommas(x) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+function callCovidApi(stateRequired, timeRequired, bodyRequired, rRateRequired, mRateRequired) {
+	$.ajax({
+		method: "GET",
+		url: "https://api.covid19india.org/data.json",
+		success: function (resData) {
 
-// Sort data by ascending order
-function sortDataByAscOrder(nodeId) {
-	var list = document.getElementById(nodeId);
+			var statewise = resData["statewise"];
 
-	var items = list.childNodes;
-	var itemsArr = [];
-	for (var i in items) {
-		if (items[i].nodeType == 1) { // get rid of the whitespace text nodes
-			itemsArr.push(items[i]);
+			for (var i = 0; i < statewise.length - 1; i++) {
+				var stateDetails = statewise[i];
+				
+				if (stateDetails["statecode"] === stateRequired) {
+					console.log('State Required:: ', stateRequired);
+					$(timeRequired).append(computeUpdatedTime(stateDetails["lastupdatedtime"]));
+
+					valueTemplate = '<td class="header-confirmed">' + numberWithCommas(stateDetails["confirmed"]) + '<br><img src="./images/triangle.png" width="10" height="10" draggable="false"/>&nbsp;<span class="delta">' + numberWithCommas(stateDetails["deltaconfirmed"]) + '</span></td>' +
+						'<td class="header-active">' + numberWithCommas(stateDetails["active"]) + '</td>' +
+						'<td class="header-recovered">' + numberWithCommas(stateDetails["recovered"]) + '<br><img src="./images/triangle.png" width="10" height="10" draggable="false"/>&nbsp;<span class="delta">' + numberWithCommas(stateDetails["deltarecovered"]) + '</span></td>' +
+						'<td class="header-dead">' + numberWithCommas(stateDetails["deaths"]) + '<br><img src="./images/triangle.png" width="10" height="10" draggable="false"/>&nbsp;<span class="delta">' + numberWithCommas(stateDetails["deltadeaths"]) + '</span></td>';
+					$(bodyRequired).append(valueTemplate);
+					$(rRateRequired).append(computeRecoveryRate(stateDetails["confirmed"], stateDetails["recovered"], stateDetails["deaths"]));
+
+					$(mRateRequired).append(computeMortalityRate(stateDetails["confirmed"], stateDetails["deaths"]));
+				}
+			}
+		},
+		error: function (xhr, status, error) {
+			alert(error);
 		}
-	}
-
-	itemsArr.sort(function (a, b) {
-		return a.innerHTML == b.innerHTML ?
-			0 :
-			(a.innerHTML > b.innerHTML ? 1 : -1);
 	});
+}
 
-	for (i = 0; i < itemsArr.length; ++i) {
-		list.appendChild(itemsArr[i]);
-	}
+function computeRecoveryRate(conf, recvr, death) {
+	return (Math.round(((recvr) / (conf - death)) * 100) + '%');
+}
+
+function computeMortalityRate(conf, death) {
+	return (Math.round((death / conf) * 100) + '%');
 }
 
 function computeUpdatedTime(updateTime) {
-	//var formatTime = updateTime.slice(3,5) + '/' 
-	+updateTime.slice(0, 2) + '/' +
+	var formatTime = updateTime.slice(3, 5) + '/' +
+		updateTime.slice(0, 2) + '/' +
 		updateTime.slice(6, updateTime.length);
 	var nowTime = new Date();
 
-	var utcUpdatedTime = new Date(updateTime);
+	var utcUpdatedTime = new Date(formatTime);
 	utcUpdatedTime = new Date(utcUpdatedTime.toUTCString().slice(0, -4));
 
 	var utcNowTime = new Date(nowTime.toUTCString().slice(0, -4));
@@ -281,23 +74,64 @@ function computeUpdatedTime(updateTime) {
 
 	if (hh === 0 && mm === 0 && ss === 0) {
 		return 'Updated now';
-	}
-	else if (hh === 0 && mm === 0 && ss === 1) {
+	} else if (hh === 0 && mm === 0 && ss === 1) {
 		return 'Updated a second ago';
-	}
-	else if (hh === 0 && mm === 0 && ss != 0) {
+	} else if (hh === 0 && mm === 0 && ss != 0) {
 		return 'Updated ' + ss + ' seconds ago';
-	}
-	else if (hh === 0 && mm === 1) {
+	} else if (hh === 0 && mm === 1) {
 		return 'Updated a minute ago';
-	}
-	else if (hh === 0 && mm != 0) {
+	} else if (hh === 0 && mm != 0) {
 		return 'Updated ' + mm + ' minutes ago';
-	}
-	else if (hh === 1) {
+	} else if (hh === 1) {
 		return 'Updated an hour ago';
-	}
-	else {
+	} else {
 		return 'Updated ' + hh + ' hours ago';
 	}
+}
+
+
+function callCovidStateApi(state, stateId) {
+	$.ajax({
+		method: "GET",
+		url: "https://api.covid19india.org/state_district_wise.json",
+		success: function (resData) {
+			var statewise = resData[state];
+			var districtDetails = statewise["districtData"];
+
+			for (var eachDistrict in districtDetails) {
+				var entry = districtDetails[eachDistrict];
+				valueTemplate = '<tr class="custom-tr"><td class="district-td">' + eachDistrict + '</td>' +
+					'<td class="district-td-data">' + entry["confirmed"] + '</td>';
+				$(stateId).append(valueTemplate);
+			}
+
+		},
+		error: function (xhr, status, error) {
+			alert(error);
+		}
+	});
+}
+
+
+function getCasesTestedCount() {
+	$.ajax({
+		method: "GET",
+		url: "https://api.covid19india.org/data.json",
+		success: function (resData) {
+			var testedOverAll = resData["tested"];
+
+			var count = testedOverAll.length;
+
+			var testedSamples = testedOverAll[count - 1]["totalsamplestested"];
+		
+
+			$("#cases-tested-in").append(testedSamples);
+			$("#cases-time-in").append(testedOverAll[count - 1]["updatetimestamp"]);
+
+
+		},
+		error: function (xhr, status, error) {
+			alert(error);
+		}
+	});
 }
